51347a6388a2a4d29aada839c05b0dfce184b9c4
JetDesktop <jethro0610@outlook.com>
2019-02-20 15:23:50 -0800

    Initialize Unreal project

---
+++ Source/CombatGame.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class CombatGameTarget : TargetRules
+{
+	public CombatGameTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "CombatGame" } );
+	}
+}
---
+++ Source/CombatGame/CombatGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATGAME_API ACombatGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
