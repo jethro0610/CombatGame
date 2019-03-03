1196e41df5c647d2543b51eadd37ec7f0531cefd
JetDesktop <jethro0610@outlook.com>
2019-03-03 14:07:13 -0800

    Add CombatCollider

---
+++ Source/CombatGame/CombatCollider.cpp
@@ -0,0 +1,56 @@
+#include "CombatCollider.h"
+
+UCombatCollider::UCombatCollider() {
+	SetCollisionProfileName("OverlapAll");
+
+	OnComponentBeginOverlap.AddDynamic(this, &UCombatCollider::OnBeginOverlap);
+}
+
+void UCombatCollider::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+ECombatColliderType UCombatCollider::GetCombatColliderType() {
+	return combatColliderType;
+}
+
+void UCombatCollider::SetCombatColliderType(ECombatColliderType newType) {
+	combatColliderType = newType;
+}
+
+bool UCombatCollider::IsIntangible() {
+	return bIntangible;
+}
+
+void UCombatCollider::SetIntangible(bool isIntangible) {
+	bIntangible = isIntangible;
+}
+
+void UCombatCollider::OnBeginOverlap(UPrimitiveComponent* overlappedComp, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult)
+{
+	if (otherActor!= nullptr && otherActor != GetOwner() && otherComp != this && otherComp != nullptr) {
+		UCombatCollider* otherCombatCollider = Cast<UCombatCollider>(otherComp);
+		if (otherCombatCollider->IsValidLowLevel()) {
+			if (GetCombatColliderType() == ECombatColliderType::Attack && otherCombatCollider->GetCombatColliderType() == ECombatColliderType::Hurt && !otherCombatCollider->IsIntangible()) {
+				OnLandAttack.Broadcast(this, otherCombatCollider, sweepResult);
+			}
+
+			if (GetCombatColliderType() == ECombatColliderType::Hurt && otherCombatCollider->GetCombatColliderType() == ECombatColliderType::Attack && !IsIntangible()) {
+				OnHitByAttack.Broadcast(this, otherCombatCollider, sweepResult);
+			}
+
+			if (GetCombatColliderType() == ECombatColliderType::Guard && otherCombatCollider->GetCombatColliderType() == ECombatColliderType::Attack) {
+				OnSuccesfulGuard.Broadcast(this, otherCombatCollider, sweepResult);
+			}
+
+			if (GetCombatColliderType() == ECombatColliderType::Attack && otherCombatCollider->GetCombatColliderType() == ECombatColliderType::Guard) {
+				OnAttackGuarded.Broadcast(this, otherCombatCollider, sweepResult);
+			}
+
+			if (GetCombatColliderType() == ECombatColliderType::Attack && otherCombatCollider->GetCombatColliderType() == ECombatColliderType::Attack) {
+				OnAttackClanked.Broadcast(this, otherCombatCollider, sweepResult);
+			}
+		}
+	}
+}
---
+++ Source/CombatGame/CombatCollider.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "CombatCollider.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FLandAttackDelegate, UCombatCollider*, attackingCollider, UCombatCollider*, hitCollider, FHitResult, hitResult);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FHitByAttackDelegate, UCombatCollider*, hitCollider, UCombatCollider*, attackingCollider, FHitResult, hitResult);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FSuccesfulGuardDelegate, UCombatCollider*, guardingCollider, UCombatCollider*, attackingCollider, FHitResult, hitResult);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttackGuardedDelegate, UCombatCollider*, attackingCollider, UCombatCollider*, guardingCollider, FHitResult, hitResult);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttackClankedDelegate, UCombatCollider*, attackingCollider, UCombatCollider*, otherAttackingCollider, FHitResult, hitResult);
+
+UENUM(BlueprintType)
+enum class ECombatColliderType : uint8 {
+	Attack	UMETA(DisplayName = "Attack"),
+	Hurt	UMETA(DisplayName = "Hurt"),
+	Guard	UMETA(DisplayName = "Guard")
+};
+
+UCLASS(Blueprintable, meta=(BlueprintSpawnableComponent))
+class COMBATGAME_API UCombatCollider : public UCapsuleComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCombatCollider();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		FName attackGroup;
+
+	UPROPERTY(EditAnywhere)
+		ECombatColliderType combatColliderType;
+
+	UPROPERTY(EditAnywhere)
+		bool bIntangible;
+
+	UPROPERTY(EditAnywhere)
+		float damage;
+
+	UPROPERTY(EditAnywhere)
+		float horizontalKnockback;
+
+	UPROPERTY(EditAnywhere)
+		float verticalKnockback;
+
+	void OnBeginOverlap(UPrimitiveComponent* overlappedComp, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult);
+
+public:
+	UPROPERTY(BlueprintAssignable)
+		FLandAttackDelegate OnLandAttack;
+
+	UPROPERTY(BlueprintAssignable)
+		FHitByAttackDelegate OnHitByAttack;
+
+	UPROPERTY(BlueprintAssignable)
+		FSuccesfulGuardDelegate OnSuccesfulGuard;
+
+	UPROPERTY(BlueprintAssignable)
+		FAttackGuardedDelegate OnAttackGuarded;
+
+	UPROPERTY(BlueprintAssignable)
+		FAttackClankedDelegate OnAttackClanked;
+
+	UFUNCTION(BlueprintCallable)
+		ECombatColliderType GetCombatColliderType();
+
+	UFUNCTION(BlueprintCallable)
+		void SetCombatColliderType(ECombatColliderType newType);
+
+	UFUNCTION(BlueprintCallable)
+		bool IsIntangible();
+
+	UFUNCTION(BlueprintCallable)
+		void SetIntangible(bool isIntangible);
+};
