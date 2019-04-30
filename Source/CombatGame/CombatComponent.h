e431987dcfe7d2fd787d0bcaa41fb0ad92902852
JetDesktop <jethro0610@outlook.com>
2019-04-30 15:40:38 -0700

    Add parent component to SpawnCombatCollider function

--- Source/CombatGame/CombatComponent.h
+++ Source/CombatGame/CombatComponent.h
@@ -42,5 +42,5 @@ public:
 		FVector GetKnockbackVector(UCombatCollider* hitCollider, UCombatCollider* attackingCollider);
 
 	UFUNCTION(BlueprintCallable)
-		void SpawnCombatCollider(FName socketName, FVector offset, float length, float width, FName attackGroup, ECombatColliderType type, bool isIntangible, float damage, float horizontalKnocback, float verticalKnockback);
+		UCombatCollider* SpawnCombatCollider(USceneComponent* parentComponent, FName socket, FVector offset, float length, float width, ECombatColliderType type, bool isIntangible, FName attackGroup, float damage, float horizontalKnocback, float verticalKnockback);
 };
